6785e7de0e98cfb4ee9f3845e312171ce4921730
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-01-18 19:58:14 -0500

    Code written for display_rules function in Game class

--- game.py
+++ game.py
@@ -5,12 +5,26 @@ class Game:
         pass
 
     def run_game(self):
-        pass
+        self.display_rules()
 
     def display_rules(self):
-        pass
+        print('Welcome to Rock Paper Scissors Lizard Spock. As seen on the Big Bang Theory!')
+        print("")
+        print('Each match will be a best of three games')
+        print('Use the number keys to enter your selction when prompted')
+        print("")
+        print('Here are the rules:')
+        print('Scissors cut Paper')
+        print('Paper covers Rock')
+        print('Rock crushes Lizard')
+        print('Lizard poisons Spock')
+        print('Spock smases Scissors')
+        print('Scissors decapitates Lizard')
+        print('Lizard eats Paper')
+        print('Paper disproves Spock')
+        print('Spock vaporizes Rock')
+        print('Rock crushes Scissors')
 
     def play_game(self):
         pass
 
-    
