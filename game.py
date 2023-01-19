0a60b8be58018500e123d27017836da150cb0c40
Jcantrell11 <118132470+Jcantrell11@users.noreply.github.com>
2023-01-18 19:08:10 -0500

    Created functions in Game class

---
+++ game.py
@@ -0,0 +1,16 @@
+
+
+class Game:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def display_rules(self):
+        pass
+
+    def play_game(self):
+        pass
+
+    
